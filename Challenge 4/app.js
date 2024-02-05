66b9113e5fb7dd1295fecb7af56068be03041b1a
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2024-02-05 12:47:48 +0530

    a

--- Challenge 4/app.js
+++ Challenge 4/app.js
@@ -1,1 +1,24 @@
-window.addEventListener("keypress")
+let a=document.querySelectorAll("div");
+window.addEventListener("keypress",check)
+
+function check()
+{
+    if(event.key=="a")
+        alert("You Pressed A");
+    else if(event.key=="s")
+        alert("You Pressed S");
+    else if(event.key=="d")
+        alert("You Pressed D");
+    else if(event.key=="f")
+        alert("You Pressed F");
+    else if(event.key=="g")
+        alert("You Pressed G");
+    else if(event.key=="h")
+        alert("You Pressed H");
+    else if(event.key=="j")
+        alert("You Pressed J");
+    else if(event.key=="k")
+        alert("You Pressed K");
+    else if(event.key=="l")
+        alert("You Pressed L");
+}
