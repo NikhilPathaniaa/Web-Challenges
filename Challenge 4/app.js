1d858f6a332a17c988c47050b2a84f73aff1ebea
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2024-02-05 14:04:16 +0530

    full working keyboard

--- Challenge 4/app.js
+++ Challenge 4/app.js
@@ -1,24 +1,60 @@
-let a=document.querySelectorAll("div");
-window.addEventListener("keypress",check)
+// let a=document.querySelectorAll("div")
+// window.addEventListener("keypress",check)
+// window.addEventListener("keyup",check1)
 
-function check()
-{
-    if(event.key=="a")
-        alert("You Pressed A");
-    else if(event.key=="s")
-        alert("You Pressed S");
-    else if(event.key=="d")
-        alert("You Pressed D");
-    else if(event.key=="f")
-        alert("You Pressed F");
-    else if(event.key=="g")
-        alert("You Pressed G");
-    else if(event.key=="h")
-        alert("You Pressed H");
-    else if(event.key=="j")
-        alert("You Pressed J");
-    else if(event.key=="k")
-        alert("You Pressed K");
-    else if(event.key=="l")
-        alert("You Pressed L");
-}
+window.addEventListener('keydown',(e)=>{
+    let a=`${e.key}`
+    document.querySelector(`#${a}`).style.border = "2px solid red";
+    console.log(a);
+}, false);
+
+window.addEventListener('keyup',(e)=>{
+    let a=`${e.key}`
+    document.querySelector(`#${a}`).style.border = "2px solid black";
+    console.log(a);
+}, false);
+
+
+
+// function check()
+// {
+//     if(event.key=="a")
+//         document.querySelector("#a").style.border = "2px solid red";
+//     else if(event.key=="s")
+//         document.querySelector("#s").style.border = "2px solid red";
+//     else if(event.key=="d")
+//         document.querySelector("#d").style.border = "2px solid red";
+//     else if(event.key=="f")
+//         document.querySelector("#f").style.border = "2px solid red";
+//     else if(event.key=="g")
+//         document.querySelector("#g").style.border = "2px solid red";
+//     else if(event.key=="h")
+//         document.querySelector("#h").style.border = "2px solid red";
+//     else if(event.key=="j")
+//         document.querySelector("#j").style.border = "2px solid red";
+//     else if(event.key=="k")
+//         document.querySelector("#k").style.border = "2px solid red";
+//     else if(event.key=="l")
+//         document.querySelector("#l").style.border = "2px solid red";
+// }
+// function check1()
+// {
+//     if(event.key=="a")
+//         document.querySelector("#a").style.border = "2px solid black";
+//     else if(event.key=="s")
+//         document.querySelector("#s").style.border = "2px solid black";
+//     else if(event.key=="d")
+//         document.querySelector("#d").style.border = "2px solid black";
+//     else if(event.key=="f")
+//         document.querySelector("#f").style.border = "2px solid black";
+//     else if(event.key=="g")
+//         document.querySelector("#g").style.border = "2px solid black";
+//     else if(event.key=="h")
+//         document.querySelector("#h").style.border = "2px solid black";
+//     else if(event.key=="j")
+//         document.querySelector("#j").style.border = "2px solid black";
+//     else if(event.key=="k")
+//         document.querySelector("#k").style.border = "2px solid black";
+//     else if(event.key=="l")
+//         document.querySelector("#l").style.border = "2px solid black";
+// }
