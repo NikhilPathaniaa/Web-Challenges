be2f0d310cd723237d01de1150e67b03cd6d81f0
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2024-02-14 14:11:46 +0530

    something something

--- Challenge 5/app.js
+++ Challenge 5/app.js
@@ -1,7 +1,28 @@
-var btn=document.querySelector(".btn");
-let x = document.getElementById("myText").value;
+let inp = document.querySelector("select")
+let btn=document.querySelector("#selectBtn")
+let h2=document.querySelector("#h2")
 
-function myFunction() {
-    let x = document.getElementById("myText").value;
-    document.getElementById("demo").innerHTML = x;
-}
+btn.addEventListener("click",()=>{
+    let dataValue=inp.value
+    if(dataValue=='USA')
+    {
+        h2.innerText=`${dataValue}`
+    }
+    else if(dataValue=='India')
+    {
+        h2.innerText=`${dataValue}`
+    }
+    else if(dataValue=='Japan')
+    {
+        h2.innerText=`${dataValue}`
+    }
+    else if(dataValue=='Paris')
+    {
+        h2.innerText=`${dataValue}`
+    }
+    else if(dataValue=='Italy')
+    {
+        h2.innerText=`${dataValue}`
+    }
+    
+})
