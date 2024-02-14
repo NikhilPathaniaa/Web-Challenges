c876ce7a614c84ef3aab376088e84fb3585bf4ea
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2024-02-14 16:05:20 +0530

    new ui on it

--- Challenge 5/app.js
+++ Challenge 5/app.js
@@ -1,28 +1,43 @@
-let inp = document.querySelector("select")
-let btn=document.querySelector("#selectBtn")
-let h2=document.querySelector("#h2")
+const apiKey ="72e82f29c6694eb0bcf91512241402";
+const apiUrl ="http://api.weatherapi.com/v1/current.json?key=72e82f29c6694eb0bcf91512241402&q=bangalore&aqi=no";
 
-btn.addEventListener("click",()=>{
-    let dataValue=inp.value
-    if(dataValue=='USA')
-    {
-        h2.innerText=`${dataValue}`
-    }
-    else if(dataValue=='India')
-    {
-        h2.innerText=`${dataValue}`
-    }
-    else if(dataValue=='Japan')
-    {
-        h2.innerText=`${dataValue}`
-    }
-    else if(dataValue=='Paris')
-    {
-        h2.innerText=`${dataValue}`
-    }
-    else if(dataValue=='Italy')
-    {
-        h2.innerText=`${dataValue}`
-    }
+async function checkWeather()
+{
+    const response = await fetch(apiUrl + ` &appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector("#h2").innerHTML=data.location.name;
+    document.getElementById("temp").innerHTML=data.current.temp_c+ "°C" ; 
+}
+
+checkWeather();
+// let inp = document.querySelector("select")   
+// let btn=document.querySelector("#selectBtn")
+// let h2=document.querySelector("#h2")
+
+// btn.addEventListener("click",()=>{
+//     let dataValue=inp.value
+//     if(dataValue=='USA')
+//     {
+//         h2.innerText=`${dataValue}`
+//     }
+//     else if(dataValue=='India')
+//     {
+//         h2.innerText=`${dataValue}`
+//     }
+//     else if(dataValue=='Japan')
+//     {
+//         h2.innerText=`${dataValue}`
+//     }
+//     else if(dataValue=='Paris')
+//     {
+//         h2.innerText=`${dataValue}`
+//     }
+//     else if(dataValue=='Italy')
+//     {
+//         h2.innerText=`${dataValue}`
+//     }
     
-})
+// })
