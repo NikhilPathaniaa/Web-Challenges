a670e597f00b63b9b371758465335b8688c4751a
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2024-02-14 12:50:12 +0530

    working on

---
+++ Challenge 5/app.js
@@ -0,0 +1,7 @@
+var btn=document.querySelector(".btn");
+let x = document.getElementById("myText").value;
+
+function myFunction() {
+    let x = document.getElementById("myText").value;
+    document.getElementById("demo").innerHTML = x;
+}
