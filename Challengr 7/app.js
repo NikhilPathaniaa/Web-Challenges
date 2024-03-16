1cd06862b401d2e76a0c537498fd8cf69a8a40db
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2024-03-16 17:40:40 +0530

    new updates

--- Challengr 7/app.js
+++ Challengr 7/app.js
@@ -1,9 +1,16 @@
 
-
+products();
 async function products()
 {
     const response = await fetch('./products.json')
     const data = await response.json();
 
-    document.write(data["icons"]) 
+    const value = data.map((i)=>
+    {
+        let value1=[];
+        value1=data[0+i-1]
+        return value1;
+    })
+    console.log(value);
+    
 }
