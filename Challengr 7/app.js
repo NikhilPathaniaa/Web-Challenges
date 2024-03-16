5785fc4d50aa3b23e1716e9b89571afae1e5cf7b
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2024-03-16 17:27:21 +0530

    new features

--- Challengr 7/app.js
+++ Challengr 7/app.js
@@ -1,15 +1,8 @@
-let btn = document.querySelector("#btn");
 
-function getInputValue(){
-    let inputVal = document.getElementById("myInput").value;
-    romun(inputVal);
-}
 
-async function romun(x)
+async function products()
 {
-    console.log(x);
-    
-    const response = await fetch('./youtube-api.json')
+    const response = await fetch('./products.json')
     const data = await response.json();
 
     document.write(data["icons"]) 
