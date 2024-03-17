b7e8f406f801dbe1abda4c4eccbcc76a449b8450
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2024-03-17 16:21:01 +0530

    added new css style

--- Challengr 7/app.js
+++ Challengr 7/app.js
@@ -1,4 +1,4 @@
-const PopPorducts = document.querySelector(".parent-card");
+const PopPorducts = document.querySelector(".most-popular-products");
 
 products();
 async function products()
@@ -9,15 +9,31 @@ async function products()
     products.forEach((product)=>
     {
         const { id, name, price, images } = product;
-        PopPorducts.innerHTML +=`<div class="product-card" data-product-id="${id}">
-                        <div class="product-card_img">
-							<img src="${images[0].url}" alt="${name}" />
-						</div>
-                        <div class="product-card_description">
-						    <div class="product-card_text">${name}</div>
-						    <div class="product-card_price">${price}</div>
-					    </div>
-        </div>`
+        PopPorducts.innerHTML +=`
+        <div class="product-card" data-product-id="${id}">
+            <div class="product-card__container">
+                <div class="product-card__btn cart" data-tooltip="add to cart"><span class="material-symbols-rounded"> shopping_bag </span></div>
+                <div class="product-card__btn fav" data-tooltip="add to wishlist"><span class="material-symbols-rounded"> favorite </span></div>
+                <div class="product-card__img">
+                    <img src="${images[0].url}" alt="${name}" />
+                </div>
+            </div>
+            <div class="product-card__description">
+                <div class="product-card__text">${name}</div>
+                <div class="product-card__price">${price}</div>
+            </div>
+
+            <div class="product-card__color">
+                ${images.map((image) => {
+                    const { url, color } = image;
+
+                    return `<button class="product-card__btn-radio" data-img="${url}">
+                                <span style="background-color: ${color};"></span>
+                            </button>`;
+                }).join("")}
+                
+            </div>
+        </div>`;
         console.log(product);
     })
     
