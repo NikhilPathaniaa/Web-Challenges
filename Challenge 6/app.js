6a9bb17a4d8f6d620cef97460aac1182049b86e8
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2024-03-21 06:37:34 +0530

    updates

--- Challenge 6/app.js
+++ Challenge 6/app.js
@@ -1,13 +1,14 @@
 let btn = document.querySelector("#btn")
-const space= document.querySelector("#jokes");
+let space= document.querySelector("#jokes");
 
 async function randomJokes()
 {
     const response = await fetch("./Jokes.json")
     const jokes = await response.json();
-
-    jokes.forEach(jokes => {
-        const {type,setup,punchline} = jokes;
-        space.innerHtml = `<div >${punchline}</div>`
-    });
+    let x = Math.floor((Math.random()) * jokes.length )
+    
+    console.log( jokes[x])
+    space.innerHTML += `<p>${jokes[x].joke}</p>`;
 }
+
+function clearSpace(){space.innerHTML=""}
