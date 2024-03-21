e5e3551a9ff0470d94be12d9072de26bb7bd7927
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2024-03-21 10:05:09 +0530

    update

--- Challenge 6/app.js
+++ Challenge 6/app.js
@@ -1,14 +1,24 @@
 let btn = document.querySelector("#btn")
-let space= document.querySelector("#jokes");
+let space = document.querySelector("#jokes")
+let emoji = document.querySelector("#emoji")
 
 async function randomJokes()
 {
     const response = await fetch("./Jokes.json")
     const jokes = await response.json();
-    let x = Math.floor((Math.random()) * jokes.length )
     
-    console.log( jokes[x])
-    space.innerHTML += `<p>${jokes[x].joke}</p>`;
+    emojis = ["😂", "😜", "😎", "😊", "🤣", "😆", "😍", "🤔", "😱", "🥳","😋", "😄", "🤗", "😇", "🤩", "🥺", "🤪", "🤓", "😏", "🤭"]
+    
+    let e = Math.floor((Math.random()) * emojis.length)
+    
+    let x = Math.floor((Math.random()) * jokes.length )
+    console.log( emojis[e])
+
+    const {type,setup,punchline} = jokes[x];
+    
+    emoji.innerHTML = `<div>${emojis[e]}</div>`
+    space.innerHTML = `<p id="setup">${setup}</p>
+                    <p id="punchline">${punchline}</p>`; 
 }
 
 function clearSpace(){space.innerHTML=""}
